6a9cd6a4b630ed3c3055514282dc0a315e34ecbb
Noxmor <janek.drews@t-online.de>
2022-08-25 14:22:54 +0200

    Added scripts

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+workspace "Optic"
+	architecture "x86_64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "Optic"
+include "Sandbox"
